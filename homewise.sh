d8ef97d86c75ac5ec82affa72cf84f38cf384531
odedtgr <oded.tgr@gmail.com>
2017-04-02 03:53:06 +0300

    sh script

--- homewise.sh
+++ homewise.sh
@@ -2,6 +2,7 @@
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
+sleep(60)
 cd /home/pi/Desktop/SmartHome
 sudo python homewise.py
 

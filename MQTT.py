1331adb8576173886516ab066be26ce039e98b9d
odedtgr <oded.tgr@gmail.com>
2017-12-07 14:43:06 +0200

    temp

--- MQTT.py
+++ MQTT.py
@@ -3,6 +3,7 @@ import json
 import ast
 from device_manager import StatusUpdater as StatusUpdater
 
+
 class MQTT:
 
     @staticmethod
@@ -20,24 +21,34 @@ class MQTT:
         slash_pos = msg.topic.index('/')
         device_address = msg.topic[slash_pos+1:len(msg.topic)]
 
-        device = self.status_updater.get_device_by_address(device_address, 1)
+        if device_address[0:len("homekit")]=="homekit":
+            slash_pos = device_address.index('/')
+            device_address = device_address[slash_pos+1:len(device_address)]
+            #device = self.status_updater.get_device_by_address(device_address, 1)
+            #device_id = device['id']
+            #self.device_manager.update_device(device_id,1, False)
 
-        if device is not None:
-            if device['type'] == 'light':
-                #convert the string to dict
-                status = json.dumps(msg.payload)
-                status = json.loads(status)
-                status = ast.literal_eval(status)
-            if status is not None:
-                self.status_updater.update_device_status(device, status)
+        else:
+            device = self.status_updater.get_device_by_address(device_address, 1)
+
+            if device is not None:
+                if device['type'] == 'light':
+                    #convert the string to dict
+                    status = json.dumps(msg.payload)
+                    status = json.loads(status)
+                    status = ast.literal_eval(status)
+                if status is not None:
+                    self.status_updater.update_device_status(device, status)
 
     def publish(self,topic, payload):
         self.client.publish(topic,payload)
 
 
     def __init__(self, broker, port, topic_sub, topic_pub, homekit_name, device_manager, logger):
+        device_manager.update_device('1', '2', '3')
         client = mqtt.Client()
         client.status_updater = StatusUpdater(device_manager)
+        client.device_manager = device_manager
         client.on_message = MQTT.on_message
         self.client = client
         self.broker = broker
